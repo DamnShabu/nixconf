4e28ed0f227b4c87a9e1a28165c78563a21d165b
DamnShabu <greeeenfirelp@gmail.com>
2026-07-02 17:11:12 +0300

    fix git identity email

--- wrappedPrograms/git.nix
+++ wrappedPrograms/git.nix
@@ -8,9 +8,9 @@
     package = pkgs.git;
     env = {
       GIT_AUTHOR_NAME = "DamnShabu";
-      GIT_AUTHOR_EMAIL = "DamnShabu@porkbuns.xyz";
+      GIT_AUTHOR_EMAIL = "greeeenfirelp@gmail.com";
       GIT_COMMITTER_NAME = "DamnShabu";
-      GIT_COMMITTER_EMAIL = "DamnShabu@porkbuns.xyz";
+      GIT_COMMITTER_EMAIL = "greeeenfirelp@gmail.com";
     };
   };
 }
